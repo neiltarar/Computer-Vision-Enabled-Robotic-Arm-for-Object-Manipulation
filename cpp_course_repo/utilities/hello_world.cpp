865aaf8e291e55cc9340c816dac99af90be4c2eb
neil tarar <neiltarar@gmail.com>
2022-09-19 22:33:02 +1000

    C++ types exercises

--- cpp_course_repo/utilities/hello_world.cpp
+++ cpp_course_repo/utilities/hello_world.cpp
@@ -2,15 +2,45 @@
 #include <iostream>
 #include <ostream>
 #include <sstream>
+#include <list>
+#include <map>
 
 int main() {
-  printf("Hello, ROS developer!   \n");
-  char d[7] = "hellos";
 
-  std::string e = "developer";
+    std::list<int> numbers_list({1,10,100,1000});
+    std::list<const char *> vocals_list( {"a","e","i","o","u"} );
+    std::list<std::string> word_list( {"test", "words", "make", "my", "list"} );
 
-  std::cout << d[0] << std::endl;
+    numbers_list.push_front(0);             //insert in the beginning
+    numbers_list.push_back(3000);           //insert in the end
+    vocals_list.push_front("i");
+    word_list.push_back("pushed back");
+
+    for (int val : numbers_list)             // Loop
+        std::cout << val << "  ";                 // Print function
 
 
-  return 0;
+    for (const char* val : vocals_list)           // Loop
+        std::cout << val << "  ";                 // Print function
+
+    for(std::string word : word_list)
+        std::cout << word << " ";
+
+    std::list<int> new_list({5,50,500});
+
+    numbers_list.insert(numbers_list.end(),new_list.begin(),new_list.end());
+
+    for (int val : numbers_list)
+        std::cout << val << " ";
+
+    std::map<std::string,int> girls_dictionary;
+    girls_dictionary["Dolores"] = 30;
+    girls_dictionary["Maeve"] = 27;
+    girls_dictionary["Theresa"] = 6;
+    girls_dictionary["Clementine"] = 11;
+
+    for (auto item : girls_dictionary)
+        std::cout << item.first << " appears in " << item.second << " episodes\n";
+
+    return 0;
 }
