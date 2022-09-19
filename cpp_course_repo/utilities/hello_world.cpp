53e4b7afbc04cc7cd5548fa1ffde870c6c9be55c
neil tarar <neiltarar@gmail.com>
2022-09-19 21:57:30 +1000

    Created Ros workspace.

---
+++ cpp_course_repo/utilities/hello_world.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <iostream>
+#include <ostream>
+#include <sstream>
+
+int main() {
+  printf("Hello, ROS developer!   \n");
+  char d[7] = "hellos";
+
+  std::string e = "developer";
+
+  std::cout << d[0] << std::endl;
+
+
+  return 0;
+}
