85378e372772ead828754c5b8a1bae4102e6139f
neil tarar <neiltarar@gmail.com>
2023-04-08 23:41:40 +1000

    Added pin labels to the stm32 IDE UI.

--- stm32_robot_arm_control/Core/Src/stm32f4xx_hal_msp.c
+++ stm32_robot_arm_control/Core/Src/stm32f4xx_hal_msp.c
@@ -135,7 +135,7 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
     PA15     ------> TIM2_CH1
     PB3     ------> TIM2_CH2
     */
-    GPIO_InitStruct.Pin = ARM2_TIM2_CH3_Pin|GPIO_PIN_11|ARM3_TIM2_CH2_Pin;
+    GPIO_InitStruct.Pin = ARM4_TIM2_CH3_Pin|CLAW7_TIM2_CH4_Pin|ARM2_TIM2_CH2_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -163,12 +163,12 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
     /**TIM3 GPIO Configuration
     PB4     ------> TIM3_CH1
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_4;
+    GPIO_InitStruct.Pin = ARM3_TIM3_CH1_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     GPIO_InitStruct.Alternate = GPIO_AF2_TIM3;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+    HAL_GPIO_Init(ARM3_TIM3_CH1_GPIO_Port, &GPIO_InitStruct);
 
   /* USER CODE BEGIN TIM3_MspPostInit 1 */
 
